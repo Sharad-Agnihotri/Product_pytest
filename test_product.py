945deed9f089e849582ef9fb626822c448ad60e1
Sharad-Agnihotri <sharadagnihotri197@gmail.com>
2025-12-01 16:04:15 +0530

    Update test_product.py

--- test_product.py
+++ test_product.py
@@ -7,5 +7,5 @@ def test_product_info():
         "Quantity: 5\n"
         "Price: 799.5"
     )
-
-    assert product_info("P102", "Keyboard", 5, 799.5) == expected_output
+    result = product_info("P102", "Keyboard", 5, 799.5)
+    assert result == expected_output
