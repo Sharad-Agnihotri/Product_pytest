d490dae85c462216938d441804ce873d168c8b8a
Sharad-Agnihotri <sharadagnihotri197@gmail.com>
2025-12-01 17:24:19 +0530

    Fix formatting in product.py output

--- product.py
+++ product.py
@@ -3,14 +3,14 @@ def product_info(product_id, name, quantity, price):
         f"Product ID: {product_id}\n"
         f"Name: {name}\n"
         f"Quantity: {quantity}\n"
-        f"Price: ₹{price:.2f}"
+        f"Price: {price}"
     )
 
-if __name__ == "__main__":
+if __name__== "__main__":
     product_id = "P102"
     name = "Keyboard"
     quantity = 5
     price = 799.50
 
     print("Product Details:\n")
-    print(product_info(product_id, name, quantity, price))
+    print(product_info(product_id, name, quantity, price))
